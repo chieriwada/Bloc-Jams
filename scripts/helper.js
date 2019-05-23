f763214772ab9fd1cdc12587fd1b4d1cf3e70dd5
Chieri Wada <wada.chieri@gmail.com>
2019-05-23 16:35:50 -0700

    Fix totalTime to update when song is changed

--- scripts/helper.js
+++ scripts/helper.js
@@ -1,6 +1,13 @@
 class Helper {
   playPauseAndUpdate (song) {
-    const totalTime = player.prettyTime(player.getDuration());
+    let totalTime;
+
+    if (song) {
+      totalTime = player.prettyTime(song.duration);
+    } else {
+      totalTime = player.prettyTime(player.getDuration());
+    };
+
     $('#time-control .total-time').text(totalTime);
     player.playPause(song);
   }
